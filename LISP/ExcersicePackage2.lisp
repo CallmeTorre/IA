db8d24d3ea8d1e2911e6b207bad5c130251e735e
CallmeTorre <alexis_torreblanca@outlook.com>
2016-08-22 09:41:30 -0500

    Exercise number 3 finished

--- LISP/ExcersicePackage2.lisp
+++ LISP/ExcersicePackage2.lisp
@@ -11,6 +11,22 @@
 
 (inicio-en '(a b c) 'a) ; (0 A B C)
 
+;3)
+(defun Termina-en (lista elem)
+       (let* ((lista_invertida (reverse lista)) ;Invertimos la Lista
+       (contador (position elem lista_invertida)) ;Obtenemos la ultima
+       incidencia de elem en la lista
+       (tamaño (- (length lista)1)) ;Obtenemos el tamaño -1 de la lista
+       (final (- tamaño contador)) ;Obtenemos hasta donde tenemos que
+       ciclar
+       (aux 0))
+       (loop for i in lista
+       	    do(setq aux(+ aux 1))
+	    collect(list i)
+	    until(equal aux final))))
+
+(TERMINA-EN '(a b c a c a) 'a) ;((A) (B) (C) (A) (C))
+
 ;4)
 (defun Primer-impar (lista)
        (let ((contador -1))
