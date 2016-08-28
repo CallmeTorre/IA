9ad8104525bb7f7524056153e8590176c56d0683
CallmeTorre <alexis_torreblanca@outlook.com>
2016-08-28 17:09:44 -0500

    New exercises added

--- LISP/ExercisePackage3.lisp
+++ LISP/ExercisePackage3.lisp
@@ -1,3 +1,43 @@
+;2)
+(defun Inicio-en(lista elem)
+      (cond ((null lista) NIL)
+      	       ((equal (first lista) elem) lista)
+	       (T (Inicio-en (rest lista) elem))))
+
+(Inicio-en '(1 2 3) '2) ; (2 3)
+
+;3)
+(defun Termina-en(lista elem)
+      (cond ((null lista) NIL)
+      	    ((equal (first lista) elem) (cons elem nil))
+	          (T (cons (first lista) (Termina-en (rest lista) elem)))))
+
+(TERMINA-EN '(1 2 3) '2) ; (1 2)
+
+;4)
+(defun Primer-impar(lista cont)
+      (cond ((null lista) nil)
+      	       ((oddp (first lista)) (cons (first lista) cont))
+	       (T (Primer-impar (rest lista) (+ cont 1)))))
+
+(Primer-impar '(2 3 4 5) 0) ; (3 . 1)
+
+;5)
+(defun ImparAux (lista)
+      (cond ((null lista) nil)
+      	       ((>= (first lista) 0) (first lista))
+	       (T (ImparAux (rest lista)))))
+
+(defun Contar (elem lista &optional cont)
+       (cond ((null lista) (list elem cont))
+       	     	((equal elem (first lista)) (Contar elem (rest lista) (+ cont 1)))
+		(T (Contar elem (rest lista) cont))))
+
+(defun Ultimo-impar (lista)
+       (Contar (ImparAux (reverse lista)) lista 0))
+
+(ultimo-impar '(1 2 1)) ; (1 2)
+
 ;16)
 (defun Encuentra(lista elem)
       (cond ((null lista) NIL)
@@ -13,6 +53,14 @@
 	          (T (cons (first lista) (Cambia (rest lista) elem1 elem2)))))
 
 (Cambia '(a b c a b c) 'a '1) ; (1 B C 1 B C)
+
+;19)
+(defun Mapea(funcion lista1 lista2)
+      (if lista1 (cons (funcall funcion (first lista1) (first lista2))
+      	  	       	   (Mapea funcion (rest lista1) (rest lista2)))
+		   NIL))
+
+(mapea #'- '(1 2 3 4) '(1 1 1 1)) ; (0 1 2 3)
 
 ;20)
 (defun Aplana(lista)
