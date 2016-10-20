57b2c9714884e4bda9db9b16df64ae15c704f88b
CallmeTorre <alexis_torreblanca@outlook.com>
2016-10-19 23:33:22 -0500

    Need to check the validations and finish the machine part

--- Exercises/Mancala.lisp
+++ Exercises/Mancala.lisp
@@ -3,6 +3,7 @@
 (defparameter *shoot-again* T)
 (defparameter *slots-shooted* nil)
 (defparameter *id* 0)
+(defparameter *infinite* most-positive-fixnum)
 (defparameter *current-ancestor* nil)
 (defparameter *ops* '((:primera-casilla 6)
                       (:segunda-casilla 5)
@@ -65,13 +66,18 @@
                    (print "~& Ya tiraste en esa casilla"))
               (push casilla-target *slots-shooted*)
               (move-ball casilla-actual casilla-target))
+         (if (= (first *slots-shooted*) 13)
+             (setq *shoot-again* T)
+             (setq *shoot-again* nil))
          (setq casilla-actual nil
                casilla-target nil
+               *slots-shooted* nil
                mValido nil
                canicas-casilla nil))))
+
 ;///////////////////////////////////////////////////
 ;Human Stuff
-;//////////////////////////////////////////////////
+;///////////////////////////////////////////////////
 
 (defun valid-operator? (operador estado)
   (let ((operador (second operador)))
@@ -90,11 +96,12 @@
           (T nil))))
 
 (defun apply-operator (operador estado)
-  (let* ((operador (first operador))
-        (casilla-actual (second operador))
-        (canicas-casilla (get-balls casilla-actual))
-        (estado-resultado nil))
-    (case operador
+  (let* ((ops (first operador))
+         (casilla-actual (second operador))
+         (canicas-casilla (get-balls casilla-actual))
+         (estado-resultado nil))
+    (print ops)
+    (case ops
       (:primera-casilla (setq estado-resultado (move-machine-balls estado casilla-actual canicas-casilla)))
       (:segunda-casilla (setq estado-resultado (move-machine-balls estado casilla-actual canicas-casilla)))
       (:tercera-casilla (setq estado-resultado (move-machine-balls estado casilla-actual canicas-casilla)))
@@ -104,12 +111,95 @@
       (T "Error"))
     estado-resultado))
 
-(defun move-machine-balls (estado casilla canicas))
+;[Check]
+(defun move-machine-balls (estado casilla canicas)
+  (let* ((estado-copia (copy-list estado))
+         (canica-copia nil)
+         (casilla-target (1- casilla))
+         (cont 0))
+    (loop for canica in canicas do
+         (setq canica-copia (pop (nth casilla estado-copia)))
+         (if (< casilla-target 0)
+             (progn
+               (setq casilla-target 7)
+               (push canica-copia (nth casilla-target estado-copia))
+               (setq casilla-target (1+ casilla-target)))
+             (progn
+               (push canica-copia (nth casilla-target estado-copia))
+               (setq casilla-target (1- casilla-target))))
+         (if (> casilla-target 13)
+             (progn
+               (setq casilla-target 6)
+               (push canica-copia (nth casilla-target estado-copia))
+               (setq casilla-target (1- casilla-target))))
+         finally (return (list estado-copia)))))
 
-(defun heuristic-function (estado))
+(defun count-pieces (casilla A V R)
+  (cond ((null casilla) (+ A V R))
+        ((equal (first casilla) 'A) (count-pieces (rest casilla) (1+ A) V R))
+        ((equal (first casilla) 'V) (count-pieces (rest casilla) A (1+ V) R))
+        ((equal (first casilla) 'R) (count-pieces (rest casilla) A V (1+ R)))
+        (T (count-pieces (rest casilla) A V R))))
 
-(defun expand (estado))
+(defun heuristic-function (estado)
+  (+ (- (count-pieces (nth 0 estado) 0 0 0)(count-pieces (nth 13 estado) 0 0 0))
+     (- (+ (count-pieces (nth 1 estado) 0 0 0)(count-pieces (nth 2 estado) 0 0 0)(count-pieces (nth 3 estado) 0 0 0)
+           (count-pieces (nth 4 estado) 0 0 0)(count-pieces (nth 5 estado) 0 0 0)(count-pieces (nth 6 estado) 0 0 0))
+        (+ (count-pieces (nth 7 estado) 0 0 0)(count-pieces (nth 8 estado) 0 0 0)(count-pieces (nth 9 estado) 0 0 0)
+           (count-pieces (nth 10 estado) 0 0 0)(count-pieces (nth 11 estado) 0 0 0)(count-pieces (nth 12 estado) 0 0 0)))))
 
-(defun machine-turn ())
+(defun change-player (jugador)
+  (case jugador
+    (0 1)
+    (1 0)))
+
+(defun expand (estado)
+  (let* ((sucesores nil)
+         (nuevo-estado nil)
+         (estado-copia (copy-list estado)))
+    (loop for operador in *ops* do
+         (if (valid-operator? operador estado-copia)
+             (progn
+               (setq nuevo-estado (apply-operator operador estado-copia))
+               (push nuevo-estado sucesores)))
+       finally (return sucesores))))
+
+(defun minimax (estado profundidad max-profundidad jugador infinito menos-infinito)
+  (if (= profundidad max-profundidad)
+      (heuristic-function estado)
+      (let ((sucesores (expand estado)))
+        (if (null sucesores)
+            (heuristic-function (first estado))
+            (do ((nuevo-valor nil)
+                 (mejor-mov (first sucesores)))
+                ((null sucesores)
+                 (if (= profundidad 0)
+                     mejor-mov
+                     menos-infinito))
+              (setf nuevo-valor
+                    (- (minimax
+                        (first sucesores)
+                        (1+ profundidad)
+                        max-profundidad
+                        (change-player jugador)
+                        (- menos-infinito)
+                        (- infinito))))
+              (when (> nuevo-valor menos-infinito)
+                (setf menos-infinito nuevo-valor)
+                (setf mejor-mov (first sucesores)))
+              (if (>= infinito menos-infinito)
+                  (setf sucesores nil)
+                  (setf sucesores (rest sucesores))))))))
+
+(defun machine-turn ()
+  (let ((movimiento nil)
+        (movimiento-final nil))
+    (loop until (null *shoot-again*) do
+         (setq movimiento (minimax *board* 0 1 1 *infinite* (- *infinite*)))
+         (setq movimiento-final (first movimiento))
+         (setq *board* movimiento-final)
+         (setq *shoot-again* nil))
+    (print-board)))
 
 (defun play ())
+
